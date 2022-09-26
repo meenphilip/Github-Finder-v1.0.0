2e1fef9ddd7a4d41e7e6c35c31246f63aa2cd606
Meen Philip <meenphilip@gmail.com>
2022-09-26 22:52:45 +0300

    init github class,select DOM element and call the get method

---
+++ script.js
@@ -0,0 +1,28 @@
+// Init Github
+const github = new Github();
+
+// search input
+const searchUser = document.getElementById("searchUser");
+
+// search input event listener
+searchUser.addEventListener("keyup", (e) => {
+  // get input text
+  searchText = e.target.value;
+  if (searchText !== "") {
+    // make http call to github API
+    github
+      .get(searchText)
+      .then((user) => {
+        if (user.profile.message === "Not Found") {
+          // show alert user not found
+        } else {
+          // show user profile
+        }
+      })
+      .catch((err) => console.error(err));
+
+    // console.log(searchText);
+  } else {
+    // clear user profile
+  }
+});
