8b2eae6948503672adba51a94cbd4ffe3cf26deb
Meen Philip <meenphilip@gmail.com>
2022-09-27 13:53:52 +0300

    create show repos

--- ui.js
+++ ui.js
@@ -49,6 +49,30 @@ class UI {
     }
   }
 
+  // Show Repos
+  showRepos(repos) {
+    let output = "";
+    repos.forEach((repo) => {
+      output += `
+      <div class="card card-body mb-2">
+        <div class="row">
+          <div class="col-md-6">
+            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+          </div>
+          <div class="col-md-6">
+          <span class="badge text-bg-primary">Stars: ${repo.stargazers_count}</span>
+          <span class="badge text-bg-secondary">Watchers: ${repo.watchers_count}</span>
+          <span class="badge text-bg-success">Forks: ${repo.forks_count}</span>
+          </div>
+        </div>
+      </div>
+      `;
+    });
+
+    //Output Repos
+    document.getElementById("repos").innerHTML = output;
+  }
+
   //Show Alert Message
   showAlert(message, className) {
     // Clear any remaining Alert
