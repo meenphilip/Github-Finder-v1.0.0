5a0da1a08967cdfd9d46b9133b7dd3b60e681206
Meen Philip <meenphilip@gmail.com>
2022-09-27 12:58:40 +0300

    add the github token

--- github.js
+++ github.js
@@ -1,13 +1,19 @@
 class Github {
   constructor() {
     this.client_id = "1b84a4152d287846a1af";
-    this.client_secret = "e92af4625335db4259b507144909cc077a2cc98e";
+    this.client_secret = "550d899c6bea14328c08ea12e1b9d22b88c8811e";
+    this.token = "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";
   }
 
   // GET USER
   async get(user) {
     const profileResponse = await fetch(
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`,
+      {
+        headers: {
+          Authorization: `token ${this.token}`,
+        },
+      },
     );
     const profile = await profileResponse.json();
 
